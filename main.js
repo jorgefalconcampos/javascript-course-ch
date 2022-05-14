2090d234f6da1e29f55c7527c876fd8cf894458a
Jorge Falcón <jorgef_campos@outlook.com>
2022-05-14 18:29:43 -0500

    manejo e interaccion con arrays

--- main.js
+++ main.js
@@ -4,36 +4,88 @@
  el total de todos los artículos
  * */ 
 
+
+
 const IVA = 16;
-let articulos = [];
+let articulos_disponibles = [];
+let articulos = []; // articulos que el usuario agrega al carrito
 let total = 0;
 
-function Articulo(id, sku, nombre, precio, impuestos, descripcion){
-    this.id = id;
-    this.sku = sku;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.impuestos = impuestos;
-    this.descripcion = descripcion;
+class Articulo {
+    constructor(id, sku, nombre, precio, impuestos, descripcion) {
+        this.id = id;
+        this.sku = sku;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.impuestos = impuestos;
+        this.descripcion = descripcion;
+        // this.cantidad = 0;
+    }
+
+    agregar(cantidad) {
+        this.cantidad = cantidad;
+    }
 }
 
-function Carrito(articulos, tieneDescuento, descuento) {
-    this.articulos = articulos;
-    this.tieneDescuento = tieneDescuento;
-    this.descuento = descuento;
+class Carrito {
+    constructor(articulos, tieneDescuento, descuento) {
+        this.articulos = articulos;
+        this.tieneDescuento = tieneDescuento;
+        this.descuento = descuento;
+    }
 }
 
 let articulo_1 = new Articulo(123, "P00JHDGNB132", "Alebrije de león", 100, IVA, "Alebrije de Oaxaca hecho a mano, tallado en madera  y pintado con finos detalles.")
 let articulo_2 = new Articulo(487, "AP826MSNDGN2", "Alebrije de toro", 320, IVA, "Alebrije hecho a mano, tallado en madera  y pintado con colores brillantes.")
 
-articulos.push(articulo_1, articulo_2);
+articulos_disponibles.push(articulo_1, articulo_2);
 
-const carrito1 = new Carrito(articulos, true, 5);
+function preguntarCliente() {
 
-console.log(carrito1);
+    const longitud = articulos_disponibles.length;
 
-for(let item=0; item<articulos.length; item++){
-    total += articulos[item].precio;
+    alert(`Actualmente, tenemos a la venta ${longitud} artículos especiales:`);
+
+    for (let item=0; item<longitud; item++){
+        alert(`Artículo ${item+1}: \n\n ${JSON.stringify(articulos_disponibles[item])}`);
+    }
+
+    let articulo = Number(prompt(`¿Qué artcículo de los ${longitud} deseas agregar?`));
+
+    switch(articulo) {
+        case 1:
+            articulos.push(articulo_1);
+            alert(`¡El ${articulo_1["nombre"]} es una buena elección!`);
+        break;
+
+        case 2:
+            articulos.push(articulo_2);
+            alert(`¡El ${articulo_2["nombre"]} es una buena elección!`);
+        break;
+
+        default:
+            alert("Al parecer no has agregado artículos a tu carrito o esta opción no está disponible...")
+        break;
+    }
+
+    
+    const carrito = new Carrito(articulos, false, 0);
+    
+    alert("Tu carrito se ve así hasta ahora: ");
+
+    for(let art=0; art<carrito["articulos"].length; art++){
+        alert(JSON.stringify(carrito["articulos"][art]));
+    }
+
+    for(let item=0; item<articulos.length; item++){
+        total += articulos[item].precio;
+    }
+
+    alert("Tu total es de: " + total)
+
+
 }
 
-console.log(total);
+let nombre = prompt("¡Hola! ¿Me puedes dar tu nombre?");
+alert(`Hola ${nombre}, estamos encantados de tenerte en nuestra tienda.`)
+preguntarCliente();
